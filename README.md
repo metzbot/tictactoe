5b67b6216b1577c6dda36490dd9d5f06f38692d9
Taylor Miller <trmiller89@gmail.com>
2022-03-23 18:00:40 -0700

    initial commit - make html structure & styling

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # tictactoe
 it's another tic tac toe rendition
+
+
+to do:
+literally everything
+
+-factory function for players
+-modules for gameBoard, displayController, game
+
+html skeleton
+
+styling
---
+++ tictactoe.js
@@ -0,0 +1,22 @@
+/*=====
+modules
+=====*/
+const gameBoard = (() => {
+  const gameArray = [];
+
+})();
+
+const gameState = (() => {
+
+})();
+
+const displayController = (() => {
+  
+})();
+
+/*============
+player factory
+============*/
+const Player = (name) => {
+  return { name };
+}
